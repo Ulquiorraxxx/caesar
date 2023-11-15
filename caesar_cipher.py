0f3831925a85af305e577734a823b8290273d613
Ulquiorraxxx <149563218+Ulquiorraxxx@users.noreply.github.com>
2023-11-15 09:39:40 +0200

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,22 +1,34 @@
-a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alfabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def e(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
-    else:
-      r += (a[(a.find(c) + o) % len(a)])
-  return r
-
-def d(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
-    else:
-      r += (a[(a.find(c) - o) % len(a)])
-  return r
+#def sifresana; t=ievaditais teksts; r=rotacijas skaitlis; Katru burtu no ievadita teksta meklē vai ir tekstā alfabets, ja tāda burta tur nav tad ievadītā teksta burts paliek tāds pats else: Azivieto to burtu ar + o vērtību nākamais burts pec alfabēta (A ir 1, B ir 2, C ir 3, utt.) un pievieno to pie izvades teksta r
+def sifresana(teksts, rotacija):
+    rezultats = ""
+    for c in teksts:
+        if (alfabets.find(c) == -1):
+            rezultats += c
+        else:
+            rezultats += (alfabets[(alfabets.find(c) + rotacija) % len(alfabets)])
+    return rezultats
+#def atsifresana; t=ievaditais teksts; r=rotacijas skaitlis; Katru burtu no ievadita teksta meklē vai ir tekstā alfabets, ja tāda burta tur nav tad ievadītā teksta burts paliek tāds pats else: Azivieto to burtu ar - o vērtību iepriekšējais burts pec alfabēta (A ir 1, B ir 2, C ir 3, utt.) un pievieno to pie izvades teksta r
+def atsifresana(teksts, rotacija):
+    rezultats = ""
+    for c in teksts:
+        if (alfabets.find(c) == -1):
+            rezultats += c
+        else:
+            rezultats += (alfabets[(alfabets.find(c) - rotacija) % len(alfabets)])
+    return rezultats
+#
+def fullatsifresana(teksts, n):
+    rezultats = ""
+    
+    for c in teksts:
+        if (alfabets.find(c) == -1):
+            rezultats += c
+        else:
+            rezultats += (alfabets[(alfabets.find(c) - n) % len(alfabets)])
+    return rezultats
+        
 
 w = """
 1. Encrypt text
@@ -24,15 +36,19 @@ w = """
 3. Bruteforce all rotations
 Choose mode: """
 mode = int(input(w))
-
+#ja ivada mode '1' tad izvada 2 tekstus (otrais parādās tikai tad, kad tiek ievaditi dati pēc pirmā teksta) un izmantojas def sifresana
 if mode == 1:
-  text = input("Enter the text: ")
-  rotation = int(input("Enter the rotation: "))
-  print("Encrypted: " + e(text, rotation))
+    text = input("Enter the text: ")
+    rotation = int(input("Enter the rotation: "))
+    print("Encrypted: " + sifresana(text, rotation))
+#ja ivada mode '2' tad izvada 2 tekstus (otrais parādās tikai tad, kad tiek ievaditi dati pēc pirmā teksta) un izmantojas def atsifresana
 elif mode == 2:
-  text = input("Enter the text: ")
-  rotation = int(input("Enter the rotation: "))
-  print("Decrypted: " + d(text, rotation))
+    text = input("Enter the text: ")
+    rotation = int(input("Enter the rotation: "))
+    print("Decrypted: " + atsifresana(text, rotation))
+#ja ievada mode=3 tad izvada 26 tekstus ar funckiju fullatsifresana
 elif mode == 3:
-  print("Bruteforcing...")
-  print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
+    text = input("enter the text: ")
+    for n in range(26):
+        
+        print(str(n+1) + ": " + fullatsifresana(text, n+1))
